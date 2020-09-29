2d8b3360923e8a6b4f38d7d9a0c1fb40c7b25c66
konvpalto <www.konvpalto@yandex.ru>
2020-09-29 16:09:02 +0500

    change on test branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hi its test branch
         </a>
       </header>
     </div>
